51db5646b3f4267eacda2361c9e8a7c34659933c
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-06-18 23:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const parseCode = (str) => {
-  // your code here
+  const parts = str.split(/0+/);
+  return {
+    firstName: parts[0],
+    lastName:  parts[1],
+    id:        parts[2],
+  };
 };
 
 // Do not change the code below
